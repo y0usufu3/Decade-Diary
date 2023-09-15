fba756a6e16059f5a8bca62cad8f9f26c98f3082
y0usuru3 <opamps4pgmail.com>
2023-09-16 04:30:37 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  root "application#hello"
+  root  'static_pages#home'
+  get   'static_pages/home'
+  get   'static_pages/help'
+  get   'static_pages/about'
+  get   'static_pages/contact'
+  
 end
