4a8472886e41442bd0d99f6d980429f994b6f450
y0usuru3 <opamps4pgmail.com>
2023-09-21 07:38:48 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# ページごとの完全なタイトルを返す
+	def full_title(page_tilte='')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_tilte.empty?
+			base_title
+		else
+			"#{page_tilte} | #{base_title}"
+		end
+	end
 end
