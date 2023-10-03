a9affbcd3c6faa54d809f990c0d52e95896e20ab
y0usuru3 <opamps4pgmail.com>
2023-10-04 07:45:31 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録完了しました。ようこそSample App!"
+      redirect_to @user
+      #下記と同等　
+      # redirect_to user_url(@user)
+    else 
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :pssword_confirmation)
+    end
 end
