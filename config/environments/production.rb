7447db2250dc58aa36e69244a6f56977dd5856d4
y0usuru3 <opamps4pgmail.com>
2023-11-02 18:26:07 +0900

    Add account activation ver4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://sample-app-vscode.onrender.com'
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port         => 587,
-    :addresses    => 'smtp.mailgun.org',
-    :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
-    :password     => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain       => host,
+    :port           => 587,
+    :address        => 'smtp.mailgun.org',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
     :authentication => :plain,
   }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
