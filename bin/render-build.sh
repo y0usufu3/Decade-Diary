dc871a939896906964d60fe4a471118d43446266
y0usuru3 <opamps4pgmail.com>
2024-03-21 23:15:38 +0900

    add db:seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,8 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 # bundle exec rails db:migrate:reset
-bundle exec rails db:migrate
+#　db:seedを有効化 bundle exec rails db:migrate
+#　db:seedを有効化で追加
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:migrate:reset 
+bundle exec rake db:seed
